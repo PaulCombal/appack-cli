060b88a86ddb0b28fcbe013fbb771f586ac7761c
PaulCombal <abonnementspaul@gmail.com>
2026-01-20 21:16:19 +0100

    Revert breaking Wayland support

--- src/utils/xdg_session_type_detector.rs
+++ src/utils/xdg_session_type_detector.rs
@@ -23,9 +23,12 @@ pub fn get_display_server() -> DisplayServer {
     })
 }
 
+// wlfreerdp3 has been deprecated. XWayland will be used for quite some time still.
+// They are working on a version using SDL. Might catch up later with that
 pub fn get_freerdp_executable() -> &'static str {
     match get_display_server() {
-        DisplayServer::Wayland => "wlfreerdp3",
+        // DisplayServer::Wayland => "wlfreerdp3",
+        DisplayServer::Wayland => "xfreerdp3",
         _ => "xfreerdp3",
     }
 }
